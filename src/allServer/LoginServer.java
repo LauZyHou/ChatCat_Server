b6abedb4b85580a6cb01c478306f29594d9e2187
刘知昊 <java233@foxmail.com>
2018-02-15 14:48:19 +0800

    获得账号密码

--- src/allServer/LoginServer.java
+++ src/allServer/LoginServer.java
@@ -14,16 +14,20 @@ public class LoginServer implements Runnable {
 	@Override
 	public void run() {
 		// 在这个[登陆请求管理线程]中,不停地监听客户端发来的登录请求
+		try {
+			ss = new ServerSocket(3838); // 3838端口
+			System.out.println("[#]开始等待客户端连接...");
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 		while (true) {
 			try {
-				ss = new ServerSocket(3838);// 3838端口
-				System.out.println("等待客户端连接...");
 				sckt = ss.accept();// 阻塞以等待连接
 				System.out.println("[+]客户端地址:" + sckt.getInetAddress());
 				// 为这个新客户建立[登陆请求处理线程]对象,启动专为这个客户服务的线程
 				new IWannaLogin(sckt).start();
 			} catch (IOException e) {
-				// 不能重复创建ServerSocket
+				e.printStackTrace();
 			}
 		}
 	}// end run()
@@ -36,6 +40,8 @@ class IWannaLogin extends Thread {
 	// 数据输入输出流
 	DataInputStream dis;
 	DataOutputStream dos;
+	// 布尔值指示是否登录成功,初始为否
+	boolean success = false;
 
 	// 构造器
 	IWannaLogin(Socket sckt) {
@@ -48,14 +54,23 @@ class IWannaLogin extends Thread {
 			// 用Socket连接对象去初始化输入输出流,将其与客户端输出输入连接
 			this.dis = new DataInputStream(sckt.getInputStream());
 			this.dos = new DataOutputStream(sckt.getOutputStream());
-			// 从输入流读入用户发来的消息
-			String str = dis.readUTF();// 阻塞发生之处
-			// 确保其消息格式,以"[login]"开头才是要登录
-			if (str.startsWith("[login]") == false)
-				return;
-			System.out.println(str);
+			// 没能输入正确的账密登录成功时,不停接收(用户可以不断尝试)
+			while (success == false) {
+				// 从输入流读入用户发来的消息
+				String str = dis.readUTF();// 阻塞发生之处
+				// 确保其消息格式,以"[login]"开头才是要登录
+				if (str.startsWith("[login]") == false)
+					return;// 如果对方Socket发来的消息格式不对,这个连接就是危险的,直接结束该线程
+				// System.out.println(str);
+				// if (str.endsWith("3838438")) {
+				String nm = str.substring(str.indexOf("]") + 1, str.indexOf("#"));
+				String pswd = str.substring(str.indexOf("#") + 1);
+				System.out.println(nm + " " + pswd);
+				success = true;
+				// }
+			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("[-]放弃登录来自:" + sckt.getInetAddress());
 		}
 	}
 }
